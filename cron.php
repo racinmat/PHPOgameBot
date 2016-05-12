ff76098848e77a9ee9b2e378ca7e3acf454ffccb
Azathoth <racinsky.matej@seznam.cz>
2016-05-12 10:44:43 +0200

    Changed time interval for starting cron job.

--- cron.php
+++ cron.php
@@ -9,10 +9,11 @@ $interval = 60; //in seconds
 while (true) {
 	$string = file_get_contents('www/cron.txt');
 	$datetime = new DateTime($string);
-	$difference = abs($datetime->getTimestamp() - (new DateTime("2016-05-11 21:06:10"))->getTimestamp());    //in seconds
-	if ($difference < $interval) {
+	$difference = (new DateTime())->getTimestamp() - $datetime->getTimestamp();    //in seconds
+	if ($difference < $interval && $difference > 0) {
 		file_put_contents('www/cron.txt', '');
-		shell_exec('php www/index.php bot:queue');
+		$output = shell_exec('php www/index.php bot:queue');
+		echo $output . PHP_EOL;
 	}
 	sleep($interval);
 }
