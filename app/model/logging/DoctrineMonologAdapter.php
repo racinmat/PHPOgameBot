24aa9f9f489fbf21656997a5bccf463fd4555525
Azathoth <racinsky.matej@seznam.cz>
2016-05-23 14:05:47 +0200

    Added whitelisting of types of logging statements.

--- app/model/logging/DoctrineMonologAdapter.php
+++ app/model/logging/DoctrineMonologAdapter.php
@@ -11,24 +11,59 @@ namespace App\Model\Logging;
 
 use Doctrine\DBAL\Logging\SQLLogger;
 use Monolog\Logger;
+use Nette\Utils\Strings;
 
 class DoctrineMonologAdapter implements SQLLogger
 {
 
+	const
+		SELECT = 'SELECT',
+		INSERT = 'INSERT',
+		UPDATE = 'UPDATE',
+		DELETE = 'DELETE',
+		MERGE = 'MERGE',
+		CALL = 'CALL',
+		EXPLAIN_PLAN = 'EXPLAIN PLAN',
+		LOCK_TABLE = 'LOCK TABLE'
+	;
+
 	/** @var  Logger */
 	private $logger;
 
-	public function __construct(Logger $logger)
+	/** @var string[] */
+	private $types;
+
+	public function __construct(Logger $logger, array $types)
 	{
 		$this->logger = $logger;
+		$this->types = $types;
 	}
 
 
 	public function startQuery($sql, array $params = null, array $types = null)
 	{
-		$paramsString = implode(', ', $params);
-		$typesString = implode(', ', $types);
-		$this->logger->addDebug("SQL: $sql, params: $paramsString, types: $typesString");
+		$toBeLogged = false;
+		foreach ($this->types as $type) {
+			if (Strings::contains($sql, $type)) {
+				$toBeLogged = true;
+				break;
+			}
+		}
+
+		if ( ! $toBeLogged) {
+			return;
+		}
+
+		$string = "SQL: $sql";
+		if ($params) {
+			$paramsString = implode(', ', $params);
+			$string .= ", params: $paramsString";
+		}
+		if ($types) {
+			$typesString = implode(', ', $types);
+			$string .= ", types: $typesString";
+		}
+		$this->logger->addDebug($string);
 	}
 
 	public function stopQuery()
